43127ee74a02d538a9381e8c72f6ad47d48c47a3
Voraxere <voraxere@gmail.com>
2023-06-08 11:51:23 -0400

    Made a 8*8 matrix, it has a cool gradient, needs pieces

--- Board.java
+++ Board.java
@@ -1,5 +1,27 @@
 public class Board {
-    public static void main(String[] args){
-        System.out.println("Make a board here");
+
+  public static void main(String[] args) {
+    CreateBoard();
+  }
+
+  private static void CreateBoard() {
+    int size = 8;
+
+    //Create the 2d array using the size
+    int board[][] = new int[size][size];
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        board[i][j] = i * j;
+      }
     }
+
+    // Print the Matrix
+    for (int i=0; i<size; i++){
+        for (int j=0; j<size; j++){
+            System.out.print(board[i][j] + "\t");
+        }
+        System.out.println();
+    }
+  }
 }
